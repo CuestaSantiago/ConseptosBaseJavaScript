c2b9ef9ef6dac2896f01b2e7e99ebb1a6f15b3d3
Santiago <gosanti988@gmail.com>
2023-03-07 03:03:16 -0500

    uso de const y let para constantes y variables

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,13 @@
+const nombre = 'Santiago'
+const apellido ='Cuesta'
+let valorDado =2
+
+
+console.log(nombre, apellido, valorDado)
+
+if (true) {
+    let valorDado =5
+    console.log(valorDado)
+}
+
+console.log(valorDado)

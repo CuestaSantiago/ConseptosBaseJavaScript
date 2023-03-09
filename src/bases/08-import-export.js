f47ea185b3488062cbe101cbfbe276afe276cf6b
Santiago <gosanti988@gmail.com>
2023-03-09 04:32:01 -0500

    uso de import y export

---
+++ src/bases/08-import-export.js
@@ -0,0 +1,16 @@
+// import { heroes } from './data/heroes'
+//imp
+import { heroes, owners } from "../data/heroes";
+
+// const getHeroeById=(id)=>{
+//     return heroes.find(hero=>hero.id===id)
+// }
+
+const getHeroeById = (id) => heroes.find((hero) => hero.id === id);
+
+// console.log(getHeroeById(2));
+
+const getHeroeByOwner = (owner) => heroes.filter((hero) => hero.owner.toLowerCase() === owner.toLowerCase());
+
+const [DC, Marvel] = owners;
+// console.log(getHeroeByOwner(Marvel))
